9b1d4a2094e9eafc8529a5ad550c1e49a74ed849
lukeburton02 <129391403+lukeburton02@users.noreply.github.com>
2025-02-10 11:11:05 +0000

    Add files via upload

---
+++ 492841_ML.R
@@ -0,0 +1,60 @@
+# ML assessment. Candidate number: 492841
+
+# Set your working directory
+setwd("C:/Users/lukeb/Downloads/LSHTM/TERM 2/Machine Learning/ML assessment")
+
+# Import relevant libraries
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(glmnet)
+
+# Read in the dataset. Empty rows are ommitted by default
+data <- read.csv("assignment2025.csv")
+
+# View the first 5 rows
+head(data, 5)
+
+# ------------------------------------
+# Section 1: Exploratory data analysis
+# ------------------------------------
+
+
+
+
+
+
+
+# ------------------------------------
+# Split into training + validation
+# ------------------------------------
+
+
+
+# ------------------------------------
+# Section 2: Regularised regression
+# ------------------------------------
+
+
+
+
+
+
+
+
+# ------------------------------------
+# Section 3: Tree-based methods
+# ------------------------------------
+
+
+
+
+
+
+
+
+
+
+# ------------------------------------
+# Section 4: Comparison of models
+# ------------------------------------
